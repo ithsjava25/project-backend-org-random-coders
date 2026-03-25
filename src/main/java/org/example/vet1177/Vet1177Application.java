4c72cad7db91027464345070ce481900d142d026
Linda Eskilsson <eskilsson.linda@hotmail.se>
2026-03-25 11:02:07 +0100

    init: add Spring Boot base project

---
+++ src/main/java/org/example/vet1177/Vet1177Application.java
@@ -0,0 +1,13 @@
+package org.example.vet1177;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vet1177Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Vet1177Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/vet1177/Vet1177ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.vet1177;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vet1177ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
