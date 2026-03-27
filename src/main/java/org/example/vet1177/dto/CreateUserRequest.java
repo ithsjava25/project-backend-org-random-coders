f545235452e2e27bcd06742b3861713458e350cb
Tatjana <malina91983@gmail.com>
2026-03-27 22:33:58 +0100

    Updates CreateUserRequest dto and pom dependency

--- src/main/java/org/example/vet1177/dto/CreateUserRequest.java
+++ src/main/java/org/example/vet1177/dto/CreateUserRequest.java
@@ -1,12 +1,24 @@
 package org.example.vet1177.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import org.example.vet1177.entities.Role;
 
 public class CreateUserRequest {
 
+    // lägger till validation i DTO
+    @NotBlank(message = "Name is required")
     private String name;
+
+    @Email(message = "Invalid email format")
+    @NotBlank(message = "Email is required")
     private String email;
+
+    @NotBlank(message = "Password is required")
     private String password;
+
+    @NotNull(message = "Role is required")
     private Role role;
 
     public String getName(){
@@ -17,4 +29,11 @@ public class CreateUserRequest {
         return email;
     }
 
+    public String getPassword() {
+        return password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
 }
