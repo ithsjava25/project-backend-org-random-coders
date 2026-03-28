37cf05e18871d256440e5e065cbbe679b4d03158
Tatjana <malina91983@gmail.com>
2026-03-28 21:28:27 +0100

    Changes exception files

--- src/main/java/org/example/vet1177/exception/GlobalExceptionHandler.java
+++ src/main/java/org/example/vet1177/exception/GlobalExceptionHandler.java
@@ -4,7 +4,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
-import java.nio.file.AccessDeniedException;
 import java.util.HashMap;
 import java.util.Map;
 
