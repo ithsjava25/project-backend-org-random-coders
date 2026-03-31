ed8523d947343c61a0a18e4101653861155a543a
Tatjana <malina91983@gmail.com>
2026-03-31 19:52:21 +0200

    Changes ActivityLogController

--- src/main/java/org/example/vet1177/controller/ActivityLogController.java
+++ src/main/java/org/example/vet1177/controller/ActivityLogController.java
@@ -1,9 +1,9 @@
 package org.example.vet1177.controller;
 
 import org.example.vet1177.dto.response.activitylog.ActivityLogResponse;
-import org.example.vet1177.entities.ActivityLog;
 import org.example.vet1177.entities.User;
 import org.example.vet1177.services.ActivityLogService;
+import org.example.vet1177.services.UserService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,20 +14,24 @@ import java.util.UUID;
 public class ActivityLogController {
 
     private final ActivityLogService activityLogService;
+    private final UserService userService;
 
-    public ActivityLogController(ActivityLogService activityLogService) {
+    public ActivityLogController(ActivityLogService activityLogService,
+                                 UserService userService) {
         this.activityLogService = activityLogService;
+        this.userService = userService;
     }
 
     // Hämta alla logs för ett MedicalRecord
     @GetMapping("/record/{recordId}")
     public List<ActivityLogResponse> getLogsByRecord(
             @PathVariable UUID recordId,
-            @RequestAttribute("currentUser") User currentUser // eller från security senare
+            @RequestHeader("userId") UUID userId
     ) {
-        List<ActivityLog> logs = activityLogService.getByRecord(recordId, currentUser);
+        User user = userService.getById(userId);
 
-        return logs.stream()
+        return activityLogService.getByRecord(recordId, user)
+                .stream()
                 .map(ActivityLogResponse::from)
                 .toList();
     }
