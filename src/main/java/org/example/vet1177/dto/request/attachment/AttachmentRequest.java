84254dc6ef309cea3f54e8c4e9b5b37900b2975e
Johan Briger <johanbriger@gmail.com>
2026-04-02 11:15:55 +0200

    feat: added AttachmentRequest

---
+++ src/main/java/org/example/vet1177/dto/request/attachment/AttachmentRequest.java
@@ -0,0 +1,16 @@
+package org.example.vet1177.dto.request.attachment;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.util.UUID;
+
+public record AttachmentRequest(
+        @NotNull(message = "Journal-ID får inte vara tomt")
+        UUID recordId,
+
+        @NotBlank(message = "Beskrivning av bilagan krävs")
+        @Size(max = 500, message = "Beskrivningen får vara max 500 tecken")
+        String description
+) {}
