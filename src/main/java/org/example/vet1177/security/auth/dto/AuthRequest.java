dfed255249519d886fd4f990540bbdc0f4deabf9
Annika Holmqvist <annika.holmqvist94@gmail.com>
2026-04-14 13:51:42 +0200

    Minor fix

--- src/main/java/org/example/vet1177/security/auth/dto/AuthRequest.java
+++ src/main/java/org/example/vet1177/security/auth/dto/AuthRequest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
+
 public record AuthRequest(
         @NotBlank(message = "Email måste anges") @Email(message = "Ogiltig emailadress") String email,
         @NotBlank(message = "Lösenord måste anges") @JsonProperty(access = JsonProperty.Access.WRITE_ONLY) String password
